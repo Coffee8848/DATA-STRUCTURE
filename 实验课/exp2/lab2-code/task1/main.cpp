5e96140a4c3f55e2f2006a189385def871607edd
AkiraCoffee <88522379+Coffee8848@users.noreply.github.com>
2024-10-14 13:53:57 +0000

    10.14 实验课/普通课

---
+++ 实验课/exp2/lab2-code/task1/main.cpp
@@ -0,0 +1,13 @@
+#include "listTemp.h"
+#include "company.h"
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	Company cmp;
+	//please test your implementation
+
+	return 0;
+}
